aa2dda361dde886fd5f85074a0710d1826db2e03
NaonLee <giraffe1254@gmail.com>
2021-01-27 15:16:28 +0900

    Count 8 - https://codingdojang.com/scode/393

---
+++ code_dojang/code_dojang/CountEight.java
@@ -0,0 +1,19 @@
+package code_dojang;
+
+//find every number including 8
+public class CountEight {
+
+	public static void main(String[] args) {
+		int count = 0;
+		
+		for(int i = 1; i <= 10000; i++) {
+			int temp = i;
+			while(temp > 0) {
+				if(temp % 10 == 8) count++;
+				temp /= 10;
+			}
+		}
+		System.out.println("Count of eight: "+ count);
+	}
+
+}
