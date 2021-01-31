8603e699ee4fa6d4f11028376ce6fa17311c2663
NaonLee <giraffe1254@gmail.com>
2021-01-31 17:55:46 +0900

    Fibonacci number

---
+++ code_dojang/code_dojang/FibonacciNumber.java
@@ -0,0 +1,24 @@
+package code_dojang;
+
+public class FibonacciNumber {
+
+	public static void main(String[] args) {
+		fibo(10);
+		fibo(20);
+		fibo(30);
+	}
+
+	public static void fibo(int n) {
+		int fir = 0;
+		int sec = 1;
+		int temp;
+		
+		while(sec < n) {
+			System.out.print(sec + " ");
+			temp = fir + sec;
+			fir =sec;
+			sec = temp;
+		}
+		System.out.println();
+	}
+}
